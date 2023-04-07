474cde5293bf908b1b33f2a81eaa95322ac669ab
krissgoshevski <97643470+krissgoshevski@users.noreply.github.com>
2023-04-07 17:18:06 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -4,19 +4,15 @@
 // asimetricna e obratno 
 require_once __DIR__ . "/constsdb.php";
 
-function encrypt($text) // ovde go enkriptiram t.e. od random text primer (hello) stanuva afksfhsdkjf 
+function encrypt($text) 
 {  
     return openssl_encrypt($text, "AES-128-ECB", ENCRYPTION_PASSWORD);
 }
 
 
-function decrypt($encrypttxt) // ovde go vrakam od afksfhsdkjf vo istiot prethoden text stanuva pak (hello)
+function decrypt($encrypttxt) 
 {
-    
     return openssl_decrypt($encrypttxt, "AES-128-ECB", ENCRYPTION_PASSWORD);
 }
 
-
-
-// stigna do 1:11:30 
 ?>
